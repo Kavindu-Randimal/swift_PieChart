939aa24a61efd5ac99a0f551b0bb6811fb4fc473
@randimalg <randimal@zorrosign.com>
2021-09-29 19:56:08 +0530

    first commit

---
+++ pieChart/PieSliceView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  pieChart
+//
+//  Created by Randimal Geeganage on 2021-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var pieSliceData:PieSliceData
+    
+    var body: some View {
+        GeometryReader{
+            geometry in Path {
+                path in
+                let width : CGFloat = min(geometry.size.width,geometry.size.height)
+                let height = width
+                
+                let center = CGPoint(x: width * 0.5, y: height * 0.5)
+                
+                path.move(to: center)
+                
+                path.addArc(center: center, radius: width * 0.5, startAngle: Angle(degrees: -90.0)+pieSliceData.startAngle, endAngle: Angle(degrees: -90.0)+pieSliceData.endAngle, clockwise: false)
+            }
+            .fill(pieSliceData.color)
+        }
+        aspectRatio(1,contentMode: .fit)
+    }
+}
+
+struct PieSliceData {
+    var startAngle: Angle
+    var endAngle: Angle
+    var color: Color
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(pieSliceData: PieSliceData(startAngle: Angle(degrees: 0.0), endAngle: Angle(degrees: 220.0), color: Color.black))
+    }
+}
---
+++ pieChart/pieChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  pieChartApp.swift
+//  pieChart
+//
+//  Created by Randimal Geeganage on 2021-06-29.
+//
+
+import SwiftUI
+
+@main
+struct pieChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
